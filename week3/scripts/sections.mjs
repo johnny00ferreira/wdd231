d1f66beffe21c3606bb30bf7f669abafdfb8e918
johnny00ferreira <johnnyferreiradasilva7@gmail.com>
2026-03-18 21:11:34 -0300

    add activity

---
+++ week3/scripts/sections.mjs
@@ -0,0 +1,10 @@
+export function setSectionSelection(sections) {
+    const sectionSelect = document.querySelector("#sectionNumber");
+
+    sections.forEach((section) => {
+        const option = document.createElement("option");
+        option.value = section.sectionNumber;
+        option.textContent = `Section ${section.sectionNumber}`;
+        sectionSelect.appendChild(option);
+    });
+}
