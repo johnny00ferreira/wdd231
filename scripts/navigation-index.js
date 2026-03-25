6b6718cc3ced9b31da028a7570c8f55350017a30
johnny00ferreira <johnnyferreiradasilva7@gmail.com>
2026-03-24 23:39:16 -0300

    add

--- scripts/navigation-index.js
+++ scripts/navigation-index.js
@@ -4,3 +4,31 @@ const nav = document.querySelector(".navigation");
 menuButton.addEventListener("click", () => {
     nav.classList.toggle("open");
 });
+const courseDetails = document.getElementById("course-details");
+
+function displayCourseDetails(course) {
+    courseDetails.innerHTML = `
+    <button id="closeModal">❌</button>
+    <h2>${course.subject} ${course.number}</h2>
+    <h3>${course.title}</h3>
+    <p><strong>Credits:</strong> ${course.credits}</p>
+    <p><strong>Certificate:</strong> ${course.certificate}</p>
+    <p>${course.description}</p>
+    <p><strong>Technologies:</strong> ${course.technology.join(", ")}</p>
+  `;
+
+    courseDetails.showModal();
+
+    document.getElementById("closeModal").addEventListener("click", () => {
+        courseDetails.close();
+    });
+
+    courseDetails.addEventListener("click", (event) => {
+        if (event.target === courseDetails) {
+            courseDetails.close();
+        }
+        courseDiv.addEventListener("click", () => {
+            displayCourseDetails(course);
+        });
+    });
+}
