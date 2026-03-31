f267850f837b92a209dc719f330ccb61448650d0
johnny00ferreira <johnnyferreiradasilva7@gmail.com>
2026-03-31 08:47:07 -0300

    add ajuste auditoria

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     document.getElementById("timestamp").value = new Date().toISOString();
+
+    document.querySelectorAll("[data-modal]").forEach(button => {
+        button.addEventListener("click", () => {
+            document.getElementById(button.dataset.modal).showModal();
+        });
+    });
+
+    document.querySelectorAll("[data-close]").forEach(button => {
+        button.addEventListener("click", () => {
+            button.closest("dialog").close();
+        });
+    });
+
 });
-
-function openModal(id) {
-    document.getElementById(id).showModal();
-}
-
-function closeModal(id) {
-    document.getElementById(id).close();
-}
